4dc61b96a928386f1f2c67f3e0686b8906d4dbae
bottlenome <bottlenome@gmail.com>
2023-08-31 23:12:58 +0900

    Add lie func variety
    add Interval checker
    add some lie funcs

--- cube_learn.py
+++ cube_learn.py
@@ -3,7 +3,22 @@ import torch.nn as nn
 import data_loader
 import math
 import sys
+from datetime import datetime, timedelta
 
+class IntervalCheckPoint():
+    def __init__(self, interval_min=10):
+        self.interval = interval_min
+        self.pre = None
+
+    def checkpoint(self):
+        if self.pre is None:
+            self.pre = datetime.now()
+            return True
+        elif self.pre + timedelta(minutes=self.interval) < datetime.now():
+            self.pre = datetime.now()
+            return True
+        else:
+            return False
 
 def is_colab():
     module_list = sys.modules
@@ -27,50 +42,109 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 class LieNet(nn.Module):
-    def __init__(self, d_model=128):
+    def __init__(self, d_model=128, mode="base"):
         super().__init__()
         self.map = nn.Linear(d_model, d_model)
         self.relu = nn.ReLU()
         self.blacket_product = nn.Linear(d_model*2, d_model)
 
-    def forward(self, src):
-        context = torch.zeros_like(src[0])
-        ret = []
-        src = self.relu(self.map(src))
-
         def blacket(a, b):
             return self.relu(self.blacket_product(torch.cat([a, b], dim = 1)))
+
+        self.blacket = blacket
+
+        if mode == "base":
+            def lie_func(i, src, context):
+                if i == 0:
+                    x = torch.zeros_like(src[0])
+                    y = src[0]
+                else:
+                    x = src[i - 1]
+                    y = src[i]
+                context = context + x + blacket(x, y)
+                return context, context
+        elif mode == "1_without_context":
+            def lie_func(i, src, context):
+                if i == 0:
+                    x = torch.zeros_like(src[0])
+                    y = src[0]
+                else:
+                    x = src[i - 1]
+                    y = src[i]
+                return context, x + blacket(x, y)
+        elif mode == "2_context_forget":
+            self.alpha = nn.Parameter(torch.tensor(1.0))
+            def lie_func(i, src, context):
+                if i == 0:
+                    x = torch.zeros_like(src[0])
+                    y = src[0]
+                else:
+                    x = src[i - 1]
+                    y = src[i]
+                context = self.alpha * context + self.blacket(x, y)
+                r = x + context
+                return context, r
+        elif mode == "3_context_forget":
+            self.alpha = nn.Parameter(torch.tensor(1.0))
+            self.beta = nn.Parameter(torch.tensor(1.0))
+            def lie_func(i, src, context):
+                if i == 0:
+                    x = torch.zeros_like(src[0])
+                    y = src[0]
+                else:
+                    x = src[i - 1]
+                    y = src[i]
+                context = self.alpha * context + self.beta * x + self.blacket(x, y)
+                r = x + context
+                return context, r
+        elif mode == "5_without_context":
+            self.alpha = nn.Parameter(torch.tensor(0.5))
+            def lie_func(i, src, context):
+                if i == 0:
+                    x = torch.zeros_like(src[0])
+                    y = src[0]
+                else:
+                    x = src[i - 1]
+                    y = src[i]
+                return context, self.alpha * x + (1 - self.alpha) * blacket(x, y)
+
+        else:
+            raise NotImplementedError(f"mode {mode} is not implemented")
+
+        self.lie_func = lie_func
+        self.mode = mode
+
+    def forward(self, src):
+        if self.mode == "4_blacket_rule":
+            context = torch.ones_like(src[0])
+        else:
+            context = torch.zeros_like(src[0])
+        ret = []
+        src = self.relu(self.map(src))
         
         for i in range(src.shape[0]):
-            # consider src[-1] is solved state
-            if i == 0:
-                x = torch.zeros_like(src[0])
-                y = src[0]
-            else:
-                x = src[i - 1]
-                y = src[i]
-            context += x + blacket(x, y)
+            context, r = self.lie_func(i, src, context)
             if i == src.shape[0] - 2:
                 a = src[0]
                 b = src[1]
                 c = src[2]
-                jacobi_identity = (blacket(a, blacket(b, c)) +
-                                   blacket(b, blacket(c, a)) +
-                                   blacket(c, blacket(a, b)))
+                jacobi_identity = (self.blacket(a, self.blacket(b, c)) +
+                                   self.blacket(b, self.blacket(c, a)) +
+                                   self.blacket(c, self.blacket(a, b)))
                 context += jacobi_identity
-            ret.append(context.clone())
+            ret.append(r.clone())
         return torch.stack(ret)
 
 
 class CubeLieNumNet(nn.Module):
-    def __init__(self, d_face=24, d_color=6, d_model=128, n_layers=1, dropout=0.0):
+    def __init__(self, d_face=24, d_color=6, d_model=128, n_layers=1, dropout=0.0, mode="base"):
         super().__init__()
         self.embed = torch.nn.Embedding(d_color + 1, d_model)
         self.pos_encoder = PositionalEncoding(d_model, dropout)
         self.compress = torch.nn.Linear(d_face * d_model, d_model)
         self.map = nn.Sequential()
         for i in range(n_layers):
-            self.map.add_module(f"lie{i}", LieNet(d_model))
+            self.map.add_module(f"lie{i}", LieNet(d_model, mode=mode))
         self.calc_num = torch.nn.Linear(d_model, 1)
         self.relu = nn.ReLU()
         self.d_face = d_face
@@ -114,11 +188,13 @@ class GPTNumNet(nn.Module):
         out = out.view(n_seq, -1)
         return out[:-1]
 
-def main(d_model=128, n_layers=3, n_epochs=100000, n_data=512000, batch_size=512, model="lie", debug_print=False):
+def main(d_model=128, n_layers=3,
+         n_interval=1000, n_epochs=100000, n_data=512000, batch_size=512,
+         model="lie", lie_mode="base", debug_print=False):
     if model == "GPT":
         model = GPTNumNet(d_model=d_model, n_layers=n_layers)
     else:
-        model = CubeLieNumNet(d_model=d_model, n_layers=n_layers)
+        model = CubeLieNumNet(d_model=d_model, n_layers=n_layers, mode=lie_mode)
     optimizer = torch.optim.AdamW(model.parameters(), lr=0.0001)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1000, gamma=0.1)
     train_loader, test_loader = data_loader.NumLoader(train_rate=0.9, batch_size=batch_size, size=n_data)
@@ -132,8 +208,9 @@ def main(d_model=128, n_layers=3, n_epochs=100000, n_data=512000, batch_size=512
         from tqdm import tqdm
     from torch.utils.tensorboard import SummaryWriter
     n_epochs = n_epochs
-    save_interval = 1000
+    save_interval = n_interval
     writer = SummaryWriter()
+    interval = IntervalCheckPoint()
 
     with tqdm(range(n_epochs)) as pbar:
         for epoch in pbar:
@@ -180,14 +257,14 @@ def main(d_model=128, n_layers=3, n_epochs=100000, n_data=512000, batch_size=512
                     test_pbar.set_postfix({'loss': loss.item() / (i + 1)})
                 writer.add_scalar("Loss/test", loss / len(test_loader), epoch)
                 writer.add_scalar("Accuracy/test", accu / len(test_loader), epoch)
-            if epoch % save_interval == 0:
+            if epoch % save_interval == 0 or interval.checkpoint():
                 save_dict = {
                     'model': model.state_dict(),
                     'optimizer': optimizer.state_dict(),
                     'scheduler': scheduler.state_dict(),
                     'epoch': epoch,
                 }
-                torch.save(save_dict, f"save/results_{epoch}.pth")
+                torch.save(save_dict, f"{writer.get_logdir()}/{epoch}.pth")
             # scheduler.step()
     save_dict = {
         'model': model.state_dict(),
@@ -195,7 +272,7 @@ def main(d_model=128, n_layers=3, n_epochs=100000, n_data=512000, batch_size=512
         'scheduler': scheduler.state_dict(),
         'epoch': epoch,
     }
-    torch.save(save_dict, f"save/results_{epoch}.pth")
+    torch.save(save_dict, f"{writer.get_logdir()}/results_{epoch}.pth")
 
 if __name__ == '__main__':
     import argparse
@@ -203,10 +280,13 @@ if __name__ == '__main__':
     parser.add_argument("--model", type=str, default="lie")
     parser.add_argument("--d_model", type=int, default=128)
     parser.add_argument("--n_layers", type=int, default=3)
+    parser.add_argument("--n_interval", type=int, default=1000)
     parser.add_argument("--n_epochs", type=int, default=100000)
     parser.add_argument("--n_data", type=int, default=512000)
     parser.add_argument("--batch_size", type=int, default=512)
     parser.add_argument("--debug_print", action="store_true")
+    parser.add_argument("--lie_mode", type=str, default="base")
     args = parser.parse_args()
 
-    main(d_model=args.d_model, n_layers=args.n_layers, n_epochs=args.n_epochs, n_data=args.n_data, batch_size=args.batch_size, model=args.model, debug_print=args.debug_print)
+    main(d_model=args.d_model, n_layers=args.n_layers, n_epochs=args.n_epochs, n_data=args.n_data, batch_size=args.batch_size,
+         model=args.model, lie_mode=args.lie_mode, debug_print=args.debug_print)
